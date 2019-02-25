37d453a23fd96347db108ebbc4c4208916db8d3b
Neto322 <42181276+Neto322@users.noreply.github.com>
2019-02-25 00:41:57 -0600

    Pos ya esta
    No sabia que habia que era obligatorio poner una descripcion yo todo stupid picandole creyendo que se subia y pos no ya tendria como 4 commits

---
+++ Assets/Movimento.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimento : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float fuerza;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(Vector2.right * fuerza, ForceMode2D.Impulse);
+    }
+}
+
+ 
---
+++ Movimiento.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float fuerza;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(Vector2.right * fuerza, ForceMode2D.Impulse);
+    }
+}
